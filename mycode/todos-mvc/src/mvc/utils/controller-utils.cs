b09b57453b48295f332dac463ea9eef982fa88d9
Pedro Frohmut <pedrofrohmut@gmail.com>
2024-04-17 19:57:42 -0300

    Fixed code from debug session

--- mycode/todos-mvc/src/mvc/utils/controller-utils.cs
+++ mycode/todos-mvc/src/mvc/utils/controller-utils.cs
@@ -6,10 +6,8 @@ public static class ControllerUtils
 {
     public static String GetConnectionString(IConfiguration config)
     {
-        // var username = config["DB_USER"];
-        // var password = config["DB_PASSWORD"];
-        var username = "postgres";
-        var password = "zigp7329";
+        var username = config["DB_USER"];
+        var password = config["DB_PASSWORD"];
         var connection_string = $"Host=localhost; Port=5009; Username={username};" +
                                 $"Password={password}; Database=goals_db";
         return connection_string;
