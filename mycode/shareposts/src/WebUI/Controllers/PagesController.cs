3a89399ecd5d52dd5e8e8105380a3b9b97e85337
Pedro Frohmut <pedrofrohmut@gmail.com>
2024-04-25 15:17:24 -0300

    1. Added MessagesPartial to show error and success messages. 2. Added Static Files support and main files for js and css. 3. Pages Controller now gets messages when using RedirectToAction from    other controllers and set the in the ViewData for the view that will    be shown

--- mycode/shareposts/src/WebUI/Controllers/PagesController.cs
+++ mycode/shareposts/src/WebUI/Controllers/PagesController.cs
@@ -5,53 +5,67 @@ namespace Shareposts.WebUI.Controllers;
 [Route("/")]
 public class PagesController : Controller
 {
+    private void SetMessagesToViewData()
+    {
+        ViewData["errorMessage"] = TempData["errorMessage"];
+        ViewData["successMessage"] = TempData["successMessage"];
+    }
+
     [HttpGet]
     [Route("/")]
     [Route("/Home")]
     public IActionResult HomePage()
     {
+        SetMessagesToViewData();
         return View("~/Views/Index.cshtml");
     }
 
     [HttpGet("/About")]
     public IActionResult AboutPage()
     {
+        SetMessagesToViewData();
         return View("~/Views/About.cshtml");
     }
 
     [HttpGet("/Users/SignIn")]
     public IActionResult SignInUserPage()
     {
+        SetMessagesToViewData();
         return View("~/Views/SignInUser.cshtml");
     }
 
     [HttpGet("/Users/SignUp")]
     public IActionResult SignUpUserPage()
     {
+        SetMessagesToViewData();
         return View("~/Views/SignUpUser.cshtml");
     }
 
     [HttpGet("/Posts/Show/{id}")]
     public IActionResult ShowPostPage(string id)
     {
+        SetMessagesToViewData();
         return View("~/Views/ShowPost.cshtml");
     }
 
     [HttpGet("/Posts/List")]
     public IActionResult PostsListPage()
     {
+        SetMessagesToViewData();
         return View("~/Views/ListPosts.cshtml");
     }
 
     [HttpGet("/Posts/Add")]
     public IActionResult AddPostPage()
     {
+        SetMessagesToViewData();
         return View("~/Views/AddPost.cshtml");
     }
 
     [HttpGet("/Posts/Edit")]
     public IActionResult EditPostPage()
     {
+        SetMessagesToViewData();
         return View("~/Views/EditPost.cshtml");
     }
 }
