18ab56a3f51769b80e8e3b930c79b86c0915cf0a
pedrofrohmut <pedrofrohmut@gmail.com>
2025-09-17 14:22:51 -0300

    Dotmastery new project: Clean Architecture Fundamental - Initial

---
+++ tutorials/dotnet-mastery/WhiteLagoon/WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Villa>? Villas { get; set; }
+}
