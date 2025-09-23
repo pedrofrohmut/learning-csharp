66f36ee336f668ddabd590d72ed088d630d6286b
pedrofrohmut <pedrofrohmut@gmail.com>
2025-09-23 13:06:58 -0300

    Updating and Deleting VillaNumbers

--- tutorials/dotnet-mastery/WhiteLagoon/WhiteLagoon.Web/Controllers/VillaNumbersController.cs
+++ tutorials/dotnet-mastery/WhiteLagoon/WhiteLagoon.Web/Controllers/VillaNumbersController.cs
@@ -15,20 +15,23 @@ public class VillaModel
 
 public class CreateVillaNumberModel
 {
-    public VillaNumber VillaNumber { get; set; }
-    public List<VillaModel> Villas { get; set; }
+    public VillaNumber VillaNumber { get; set; } = new VillaNumber();
+    public List<VillaModel> Villas { get; set; } = new List<VillaModel>();
+}
 
-    public CreateVillaNumberModel()
-    {
-        this.VillaNumber = new VillaNumber();
-        this.Villas = new List<VillaModel>();
-    }
+public class UpdateVillaNumberModel
+{
+    public int OldVillaNumber { get; set; }
+    public VillaNumber VillaNumber { get; set; } = new VillaNumber();
+    public List<VillaModel> OtherVillas { get; set; } = new List<VillaModel>();
+}
 
-    public CreateVillaNumberModel(List<VillaModel>? villas)
-    {
-        this.VillaNumber = new VillaNumber();
-        this.Villas = villas ?? new List<VillaModel>();
-    }
+public class DeleteVillaNumberModel
+{
+    public int Id { get; set; }
+    public int Number { get; set; }
+    public string SpecialDetails { get; set; } = "";
+    public string VillaName { get; set; } = "";
 }
 
 public class VillaNumbersController : Controller
@@ -50,10 +53,12 @@ public class VillaNumbersController : Controller
     [HttpGet]
     public IActionResult Create()
     {
-        var villaModels = this.dbContext.Villas
-            .Select(x => new VillaModel { Id = x.Id, Name = x.Name })
-            .ToList();
-        return View(new CreateVillaNumberModel(villaModels));
+        var villas = this.dbContext.Villas.Select(x => new VillaModel { Id = x.Id, Name = x.Name }).ToList();
+        var viewModel = new CreateVillaNumberModel {
+            VillaNumber = new VillaNumber(),
+            Villas = villas ?? new List<VillaModel>(),
+        };
+        return View(viewModel);
     }
 
     [HttpPost]
@@ -86,55 +91,82 @@ public class VillaNumbersController : Controller
         return RedirectToAction("Index", "VillaNumbers");
     }
 
-    // [HttpGet]
-    // public IActionResult Update(int villaId)
-    // {
-    //     var villa = this.dbContext.Villas?.FirstOrDefault(x => x.Id == villaId);
-    //     if (villa == null) {
-    //         TempData["error"] = "Villa not found with this id";
-    //         return RedirectToAction("Error", "Home");
-    //     }
-    //     return View(villa);
-    // }
+    [HttpGet]
+    public IActionResult Update(int villaNumberId)
+    {
+        var villaNumber = this.dbContext.VillaNumbers.Include(x => x.Villa).FirstOrDefault(x => x.Id == villaNumberId);
+        if (villaNumber == null) {
+            TempData["error"] = "Villa number not found with the passed id";
+            return RedirectToAction("Index", "VillaNumbers");
+        }
 
-    // [HttpPost]
-    // public IActionResult Update(Villa updatedVilla)
-    // {
-    //     if (updatedVilla.Name == updatedVilla.Description) {
-    //         // Leave blank the be ModelOnly or provide a name if targeting a field
-    //         ModelState.AddModelError("Name", "The description cannot be the same of the name");
-    //     }
-    //     if (!ModelState.IsValid) {
-    //         return View();
-    //     }
-    //     if (updatedVilla.Id == 0) {
-    //         TempData["error"] = "There is no id for the villa. Could not update model";
-    //         return RedirectToAction("Error", "Home");
-    //     }
-    //     this.dbContext.Update(updatedVilla);
-    //     this.dbContext.SaveChanges();
-    //     TempData["success"] = "The villa was updated successfully";
-    //     return RedirectToAction("Index", "Villas");
-    // }
+        // Get other villas to populate the select with the other options
+        var otherVillas = this.dbContext.Villas.Where(x => x.Id != villaNumber.VillaId)
+            .Select(x => new VillaModel { Id = x.Id, Name = x.Name }).ToList();
 
-    // [HttpGet]
-    // public IActionResult Delete(int villaId)
-    // {
-    //     var villa = this.dbContext.Villas?.FirstOrDefault(x => x.Id == villaId);
-    //     if (villa == null) {
-    //         TempData["error"] = "Villa not found with this id";
-    //         return RedirectToAction("Error", "Home");
-    //     }
-    //     return View(villa);
-    // }
+        var viewModel = new UpdateVillaNumberModel {
+            OldVillaNumber = villaNumber.Number,
+            VillaNumber = villaNumber,
+            OtherVillas = otherVillas ?? new List<VillaModel>(),
+        };
+        return View("UpdateVillaNumber", viewModel);
+    }
 
+    [HttpPost]
+    public IActionResult Update(UpdateVillaNumberModel model)
+    {
+        // Check villa exists
+        var villaExists = this.dbContext.Villas.Any(x => x.Id == model.VillaNumber.VillaId);
+        if (!villaExists) {
+            TempData["error"] = "Villa with this Id does not exist";
+            return RedirectToAction("Update", "VillaNumbers", new { villaNumberIs = model.VillaNumber.Id });
+        }
 
-    // [HttpPost]
-    // public IActionResult Delete(Villa villaToDelete)
-    // {
-    //     this.dbContext.Villas?.Remove(villaToDelete);
-    //     this.dbContext.SaveChanges();
-    //     TempData["success"] = "The villas was deleted successfully";
-    //     return RedirectToAction("Index", "Villas");
-    // }
+        // Check number is available only if it changed
+        if (model.OldVillaNumber != model.VillaNumber.Number) {
+            var numberTaken = this.dbContext.VillaNumbers.Any(x => x.Number == model.VillaNumber.Number);
+            if (numberTaken) {
+                TempData["error"] = "Villa number already taken";
+                return RedirectToAction("Update", "VillaNumbers", new { villaNumberId = model.VillaNumber.Id });
+            }
+        }
+
+        this.dbContext.Update(model.VillaNumber);
+        this.dbContext.SaveChanges();
+        TempData["success"] = "The villa numbers was updated successfully";
+        return RedirectToAction("Index", "VillaNumbers");
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int villaNumberId)
+    {
+        var villaNumber = this.dbContext.VillaNumbers.Include(x => x.Villa).FirstOrDefault(x => x.Id == villaNumberId);
+        if (villaNumber == null) {
+            TempData["error"] = "Villa Number not found with this id";
+            return RedirectToAction("Error", "Home");
+        }
+
+        var viewModel = new DeleteVillaNumberModel {
+            Id = villaNumber.Id,
+            Number = villaNumber.Number,
+            SpecialDetails = villaNumber.SpecialDetails ?? "",
+            VillaName = villaNumber.Villa?.Name ?? "",
+        };
+        return View("DeleteVillaNumber", viewModel);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(DeleteVillaNumberModel model)
+    {
+        var exists = this.dbContext.VillaNumbers.Any(x => x.Id == model.Id);
+        if (!exists) {
+            TempData["error"] = "No villa number found by the passed id";
+            return RedirectToAction("Index", "VillaNumbers");
+        }
+
+        this.dbContext.VillaNumbers.Remove(new VillaNumber { Id = model.Id });
+        this.dbContext.SaveChanges();
+        TempData["success"] = "The villa number was deleted successfully";
+        return RedirectToAction("Index", "VillaNumbers");
+    }
 }
